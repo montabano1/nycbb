ce6a8f7c77a9f7f00535b8bdc06a7dccd603b16a
Michael Montalbano <montabano1@aol.com>
2023-10-08 00:08:07 -0400

    notification not alert

--- functions/index.js
+++ functions/index.js
@@ -37,7 +37,7 @@ exports.verifyPaymentAndProvideDownloadLink = functions.https.onCall(async (data
             // Optionally: Further check if this session corresponds to the correct product, amount, etc.
 
             // Provide the download link if payment is verified
-            return { downloadLink: 'https://firebasestorage.googleapis.com/v0/b/nycbb-f8a89.appspot.com/o/Monte%20Scraping%20Real%20Estate%20-%2010_3_23.csv?alt=media&token=885768fa-3e55-4ff0-a747-f84de2409408&_gl=1*1burskx*_ga*MTQ5MDY1MTQ4NC4xNjU0MTQwMzI1*_ga_CW55HF8NVT*MTY5NjczMzc5NS4xNTUuMS4xNjk2NzM0MDkzLjU5LjAuMA..' };
+            return { downloadLink: 'https://firebasestorage.googleapis.com/v0/b/nycbb-f8a89.appspot.com/o/NYCBBlist.csv?alt=media&token=86549146-09a7-40a5-971c-859387572e86&_gl=1*g2w5gj*_ga*MTQ5MDY1MTQ4NC4xNjU0MTQwMzI1*_ga_CW55HF8NVT*MTY5NjczMzc5NS4xNTUuMS4xNjk2NzM3NjA4LjYwLjAuMA..' };
         } else {
             // Handle the case where the payment is not successful
             return { error: 'Payment not successful' };

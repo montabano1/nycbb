a93a7299e7ac284811a815a3fb08dea3675cf0c4
Michael Montalbano <montabano1@aol.com>
2023-11-21 15:15:51 -0500

    new site

---
+++ js/checkout.js
@@ -0,0 +1,200 @@
+$(document).ready(function() {
+  const stripe = Stripe('pk_live_51O0FmtDhrn1JbalvsKHCX0QXFxMnTkLI3NfpbK19pdiN7FSghO5S1b3DMXqXeiSIA3TAo0un9htxlY6DxUvhiZGI00N0SNzcTs');
+  const elements = stripe.elements();
+
+  const itemsData = {
+    'one-time': {
+      imageUrl: 'img/item1.png',
+      price: '$175',
+      description: 'Receive your list today'
+    },
+    'semi-annual': {
+      imageUrl: 'img/item2.png',
+      price: '$240',
+      description: 'Receive your list today + an updated list in 6 months'
+    },
+    'quarterly': {
+      imageUrl: 'img/item3.png',
+      price: '$350',
+      description: 'Receive your list today + updated lists every 3 months (3 more updates)'
+    }
+  };
+
+  function getPlanFromUrl() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('plan'); // Assuming your URL parameter is named 'plan'
+  }
+
+  function updateMonteDiv(planId) {
+    const data = itemsData[planId];
+
+    if (data) {
+      $('.monteImage').attr('src', data.imageUrl);
+      $('.montePrice').text(data.price);
+      $('.monteDescription').text(data.description);
+    }
+  }
+
+  const selectedPlan = getPlanFromUrl();
+  if (selectedPlan) {
+    $(`#${selectedPlan}`).prop('checked', true).trigger('change');
+  }
+
+  // Event handler for radio button change
+  $('input[type="radio"][name="plan"]').change(function() {
+    const planId = $(this).attr('id');
+    updateMonteDiv(planId);
+  });
+
+  if (selectedPlan && itemsData[selectedPlan]) {
+    updateMonteDiv(selectedPlan); // Update monte div with the selected plan data
+    $(`input[type="radio"][name="plan"][id="${selectedPlan}"]`).prop('checked', true);
+  }
+
+  $('input[type="radio"][name="plan"]').change(function() {
+    const planId = $(this).attr('id');
+    const data = itemsData[planId];
+
+    if (data) {
+      // Update the image source
+      $('.monteImage').attr('src', data.imageUrl);
+
+      // Update the price text
+      $('.montePrice').text(data.price);
+
+      // Update the description text
+      $('.monteDescription').text(data.description);
+    }
+  });
+
+  const style = {
+    base: {
+      color: '#32325d',
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: 'antialiased',
+      fontSize: '16px',
+      '::placeholder': {
+        color: '#aab7c4'
+      }
+    },
+    invalid: {
+      color: '#fa755a',
+      iconColor: '#fa755a'
+    }
+  };
+
+  const cardNumber = elements.create('cardNumber', { style: style });
+  cardNumber.mount('#cardNumber');
+
+  const cardExpiry = elements.create('cardExpiry', { style: style });
+  cardExpiry.mount('#cardExpiry');
+
+  const cardCvc = elements.create('cardCvc', { style: style });
+  cardCvc.mount('#cardCvc');
+
+  // Handle real-time validation errors from the card Element
+  function displayError(elementId, errorMessage) {
+    $('#' + elementId).text(errorMessage || '');
+  }
+
+  cardNumber.on('change', function(event) {
+    displayError('card-number-errors', event.error ? event.error.message : '');
+  });
+
+  cardExpiry.on('change', function(event) {
+    displayError('card-expiry-errors', event.error ? event.error.message : '');
+  });
+
+  cardCvc.on('change', function(event) {
+    displayError('card-cvc-errors', event.error ? event.error.message : '');
+  });
+
+
+
+  $('#submitBtn').on('click touchstart', function(e) {
+    e.preventDefault();
+    $('#spinner').show();
+
+    var isValid = true;
+
+    // Input fields
+    var email = $('#email').val();
+    var fullName = $('#fname').val();
+    var address = $('#address').val();
+    // ... add other field variables as needed
+
+    // Field validation
+    if (email.length === 0 || !email.includes('@')) {
+      isValid = false;
+      $('#email-error').text('Please enter a valid email address.'); // Display email error
+    }
+
+    if (fullName.length === 0) {
+      isValid = false;
+      $('#fname-error').text('Please enter your full name.'); // Display full name error
+    }
+
+    if (address.length === 0) {
+      isValid = false;
+      $('#address-error').text('Please enter your address.'); // Display address error
+    }
+
+    var selectedPriceId = $('input[name="plan"]:checked').data('priceid');
+
+    // Create Payment Intent on the server
+    if (isValid) {
+      var createPaymentIntent = firebase.functions().httpsCallable('createPaymentIntent');
+      createPaymentIntent({ email: email, priceId: selectedPriceId })
+      .then(function(result) {
+        if (result.error) {
+          // Show error to the customer
+          console.error('Payment failed:', result.error.message);
+          $('#spinner').hide();
+        }
+        // Confirm the payment with the card details
+        return stripe.confirmCardPayment(result.data.clientSecret, {
+          payment_method: {
+            card: cardNumber, // Use the cardNumber Element
+            billing_details: {
+              email: email
+            }
+          }
+        });
+      })
+      .then(function(result) {
+        if (result.error) {
+          // Show error to the customer
+          console.error('Payment failed:', result.error.message);
+          $('#spinner').hide();
+        } else {
+          if (result.paymentIntent.status === 'succeeded') {
+            $('#preContainer').hide();
+            $('#success-message').show(); // Make sure you have this element in your HTML
+          }
+        }
+      })
+      .catch(function(error) {
+        console.error('Error:', error);
+        $('#spinner').hide();
+      });
+    }
+
+  });
+
+  function validateEmail(email) {
+    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  }
+
+  $('#terms-checkbox').change(function(){
+    if(this.checked) {
+      $('#submitBtn').prop('disabled', false);
+    } else {
+      $('#submitBtn').prop('disabled', true);
+    }
+  });
+
+  $('.close-btn').on('click', function() {
+    window.location.href = '/'; // Replace '/' with your home page URL if different
+  });
+});
--- js/main.js
+++ js/main.js
@@ -128,103 +128,103 @@ jQuery(document).ready(function ($) {
   // custom code monte
   var selectedPriceId;  // Global variable to store the selected priceId
 
-$('button[id^="purchaseBtn"]').on('click', function() {
-    selectedPriceId = $(this).data('priceid');  // Store the priceId when a button is clicked
-    $('#emailModal').show();
-});
-
-$('.close').on('click', function() {
-    $('#emailModal').hide();
-});
-
-function validateEmail(email) {
-    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(String(email).toLowerCase());
-}
-
-$('#submitEmailBtn').on('click touchstart', function(e) {
-    e.stopPropagation();
-
-    var email = $('#emailInput').val();
-    if (validateEmail(email)) {
-        $('#emailModal').hide();
-        $('#spinner').show();
-
-        console.log("Fetched priceId:", selectedPriceId);  // Debugging statement
-
-        var functions = firebase.functions();
-        var createCheckoutSession = functions.httpsCallable('createCheckoutSession');
-
-        createCheckoutSession({ email: email, priceId: selectedPriceId })
-  .then(function(result) {
-    console.log(result)
-    $('#spinner').hide();
-    // Redirect to Stripe Checkout using session ID received from Cloud Function
-    var stripe = Stripe('pk_live_51O0FmtDhrn1JbalvsKHCX0QXFxMnTkLI3NfpbK19pdiN7FSghO5S1b3DMXqXeiSIA3TAo0un9htxlY6DxUvhiZGI00N0SNzcTs');
-    stripe.redirectToCheckout({ sessionId: result.data.sessionId })
-      .then(function (result) {
-        if (result.error) {
-          alert(result.error.message);
-        }
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  })
-  .catch(function(error) {
-    console.error('Error:', error);
-  });
-    } else {
-      alert('Please enter a valid email address.');
-    }
-  });
+  // $('button[id^="purchaseBtn"]').on('click', function() {
+  //   selectedPriceId = $(this).data('priceid');  // Store the priceId when a button is clicked
+  //   $('#emailModal').show();
+  // });
+  //
+  // $('.close').on('click', function() {
+  //   $('#emailModal').hide();
+  // });
+  //
+  // function validateEmail(email) {
+  //   var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  //   return re.test(String(email).toLowerCase());
+  // }
+  //
+  // $('#submitEmailBtn').on('click touchstart', function(e) {
+  //   e.stopPropagation();
+  //
+  //   var email = $('#emailInput').val();
+  //   if (validateEmail(email)) {
+  //     $('#emailModal').hide();
+  //     $('#spinner').show();
+  //
+  //     console.log("Fetched priceId:", selectedPriceId);  // Debugging statement
+  //
+  //     var functions = firebase.functions();
+  //     var createCheckoutSession = functions.httpsCallable('createCheckoutSession');
+  //
+  //     createCheckoutSession({ email: email, priceId: selectedPriceId })
+  //     .then(function(result) {
+  //       console.log(result)
+  //       $('#spinner').hide();
+  //       // Redirect to Stripe Checkout using session ID received from Cloud Function
+  //       var stripe = Stripe('pk_live_51O0FmtDhrn1JbalvsKHCX0QXFxMnTkLI3NfpbK19pdiN7FSghO5S1b3DMXqXeiSIA3TAo0un9htxlY6DxUvhiZGI00N0SNzcTs');
+  //       stripe.redirectToCheckout({ sessionId: result.data.sessionId })
+  //       .then(function (result) {
+  //         if (result.error) {
+  //           alert(result.error.message);
+  //         }
+  //       })
+  //       .catch(function (error) {
+  //         console.error(error);
+  //       });
+  //     })
+  //     .catch(function(error) {
+  //       console.error('Error:', error);
+  //     });
+  //   } else {
+  //     alert('Please enter a valid email address.');
+  //   }
+  // });
 
   function getUrlParameter(name) {
-        var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.search);
-        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-    }
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  }
 
-    // Check URL parameters
-    var paymentStatus = getUrlParameter('payment');
-    var sessionId = getUrlParameter('session_id');
-    var email = getUrlParameter('email')
+  // Check URL parameters
+  var paymentStatus = getUrlParameter('payment');
+  var sessionId = getUrlParameter('session_id');
+  var email = getUrlParameter('email')
 
-    if (paymentStatus === "success" && sessionId) {
-        // You can further validate the session ID if needed
-        // For this example, we'll just check its existence
+  if (paymentStatus === "success" && sessionId) {
+    // You can further validate the session ID if needed
+    // For this example, we'll just check its existence
 
-        // Set the success message
-        var message = "Successful checkout. An email with the attached spreadsheet has been sent to " + email;
-        $("#successMessage").text(message);
+    // Set the success message
+    var message = "Successful checkout. An email with the attached spreadsheet has been sent to " + email;
+    $("#successMessage").text(message);
 
-        // Display the modal
-        $("#successModal").show();
-    }
+    // Display the modal
+    $("#successModal").show();
+  }
 
-    // Close modal on button click
-    $("#closeModalBtn").click(function() {
-        $("#successModal").hide();
+  // Close modal on button click
+  $("#closeModalBtn").click(function() {
+    $("#successModal").hide();
+  });
+
+  $('#supportForm').submit(function(event) {
+    event.preventDefault();
+    console.log($(this).serialize());
+    $.ajax({
+      url: 'https://us-central1-nycbb-f8a89.cloudfunctions.net/sendEmail',
+      type: 'POST',
+      data: $(this).serialize(),
+      success: function(data) {
+        console.log('Email sent successfully');
+        $("#sendmessage").addClass("show");
+        $("#errormessage").removeClass("show");
+        $('.contactForm').find("input, textarea").val("");
+      },
+      error: function(error) {
+        console.error('Error sending email', error);
+        $("#sendmessage").removeClass("show");
+        $("#errormessage").addClass("show");
+        $('#errormessage').html("error");
+      }
     });
-
-    $('#supportForm').submit(function(event) {
-        event.preventDefault();
-        console.log($(this).serialize());
-        $.ajax({
-            url: 'https://us-central1-nycbb-f8a89.cloudfunctions.net/sendEmail',
-            type: 'POST',
-            data: $(this).serialize(),
-            success: function(data) {
-                console.log('Email sent successfully');
-                $("#sendmessage").addClass("show");
-                $("#errormessage").removeClass("show");
-                $('.contactForm').find("input, textarea").val("");
-            },
-            error: function(error) {
-                console.error('Error sending email', error);
-                $("#sendmessage").removeClass("show");
-                $("#errormessage").addClass("show");
-                $('#errormessage').html("error");
-            }
-        });
-    });
+  });
 });
